2d381674c485686bccc444fdf6c3863aa7f06bc2
Jia Yuan Teoh <yuan@jias-air.myfiosgateway.com>
2020-11-07 15:32:23 -0500

    update sidebar

--- content.js
+++ content.js
@@ -10,11 +10,12 @@ function grabTitle(){
 	}
 }
 
+var link="";
 
 function onWindowLoad(){
 	console.log("welcome here!");
-
-	var link= window.location.toString();
+	link= window.location.toString();
+	console.log(link);
 	if (link.includes("?ps=1")){
 		grabTitle();
 
@@ -35,10 +36,18 @@ function onWindowLoad(){
 		});
 
 	}else{
-			
+		var modal = document.getElementById("myModal");
+		if (modal != null){
+			modal.style.display="none";
+		}
 		console.log('not in the page');
 	}
 }
 
 window.onload = onWindowLoad;
-window.addEventListener("click", function(){ onWindowLoad(); });
+window.addEventListener("click", function(){
+	var newLink = window.location.toString();
+	if(link != newLink){
+		onWindowLoad(); 
+	}
+});
--- sidebar.js
+++ sidebar.js
@@ -5,6 +5,5 @@ var span = document.getElementById("model-close-button");
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-	console.log('click button');
   modal.style.display = "none";
 }
